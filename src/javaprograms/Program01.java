a89ac9a6fb4a4264a78b7b95e94d55bdff8c4792
rohit-basra <basra.rohit@gmail.com>
2021-04-02 19:31:00 -0400

    Program02

---
+++ src/javaprograms/Program01.java
@@ -0,0 +1,15 @@
+package javaprograms;
+/**
+ * Program 01: Write a program to display/print your name.
+ * @author StudyViral
+ *
+ **/
+public class Program01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Rohit Basra");
+
+	}
+
+}
---
+++ src/javaprograms/Program02.java
@@ -0,0 +1,21 @@
+package javaprograms;
+/**
+ * Program02: Program to Add Two Integers
+ * @author StudyViral
+ *
+ */
+public class Program02 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers");
+	    int num1 = 10;
+	    int num2 = 20;
+	    //Display numbers
+	    System.out.println(num1 + " " + num2);
+
+	    //Add two numbers
+	    int sum = num1 + num2;
+	    System.out.println("The sum is: " + sum);
+	}
+
+}
